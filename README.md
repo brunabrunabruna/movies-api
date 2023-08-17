a5ca91aab02a3cb5bb31bb99a96224c6b4a75c1c
brunabrunabruna <brunaandreis@hotmail.com>
2023-08-17 16:37:56 +0200

    implemented endpoints

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # movies-api
+
+idea: make option for user to get a randomised movie, either from a specific category( thriller, romance, comedy...), or a random option from all the movies. FOr people that dont want to spend hours browsing and choosing from netflix. They can just click random and watch that movie instead.
--- index.js
+++ index.js
@@ -2,48 +2,158 @@ const express = require("express");
 const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
-const app = express();
+const { request } = require("http");
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
 
-const moviesList = [
+const app = express();
+app.use(bodyParser.json());
+
+let users = [
+  { id: 1, name: "kim", favoriteMovies: [] },
+  { id: 2, name: "joe", favoriteMovies: [] },
+];
+
+let movies = [
   {
-    title: "star wars",
-    director: "director 1",
+    title: "starwars",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    genre: { name: "drama", description: "genre description here" },
+    director: {
+      name: "director1",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      born: 1940,
+      death: "",
+    },
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foriginalvintagemovieposters.com%2Fwp-content%2Fuploads%2F2018%2F06%2FStar-Wars-5672-768x1162.jpg&f=1&nofb=1&ipt=d191eb7a7592ede00a4efb69d08105b59832ac04e24f4d6e8eab7df59bc5537b&ipo=images",
+    feature: true,
   },
   {
     title: "film 2",
-    director: "director 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    genre: { name: "thriller", description: "genre description here" },
+    director: {
+      name: "director 1",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      born: 1940,
+      death: "",
+    },
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foriginalvintagemovieposters.com%2Fwp-content%2Fuploads%2F2018%2F06%2FStar-Wars-5672-768x1162.jpg&f=1&nofb=1&ipt=d191eb7a7592ede00a4efb69d08105b59832ac04e24f4d6e8eab7df59bc5537b&ipo=images",
+    feature: true,
   },
   {
     title: "film 3",
-    director: "director 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    genre: { name: "thriller", description: "genre description here" },
+    director: {
+      name: "director 1",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      born: 1940,
+      death: "",
+    },
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foriginalvintagemovieposters.com%2Fwp-content%2Fuploads%2F2018%2F06%2FStar-Wars-5672-768x1162.jpg&f=1&nofb=1&ipt=d191eb7a7592ede00a4efb69d08105b59832ac04e24f4d6e8eab7df59bc5537b&ipo=images",
+    feature: true,
   },
   {
     title: "film 4",
-    director: "director 4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    genre: { name: "thriller", description: "genre description here" },
+    director: {
+      name: "director 1",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      born: 1940,
+      death: "",
+    },
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foriginalvintagemovieposters.com%2Fwp-content%2Fuploads%2F2018%2F06%2FStar-Wars-5672-768x1162.jpg&f=1&nofb=1&ipt=d191eb7a7592ede00a4efb69d08105b59832ac04e24f4d6e8eab7df59bc5537b&ipo=images",
+    feature: true,
   },
   {
     title: "film 5",
-    director: "director 5",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    genre: { name: "thriller", description: "genre description here" },
+    director: {
+      name: "director 1",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      born: 1940,
+      death: "",
+    },
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foriginalvintagemovieposters.com%2Fwp-content%2Fuploads%2F2018%2F06%2FStar-Wars-5672-768x1162.jpg&f=1&nofb=1&ipt=d191eb7a7592ede00a4efb69d08105b59832ac04e24f4d6e8eab7df59bc5537b&ipo=images",
+    feature: true,
   },
   {
     title: "film 6",
-    director: "director 6",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    genre: { name: "thriller", description: "genre description here" },
+    director: {
+      name: "director 1",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      born: 1940,
+      death: "",
+    },
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foriginalvintagemovieposters.com%2Fwp-content%2Fuploads%2F2018%2F06%2FStar-Wars-5672-768x1162.jpg&f=1&nofb=1&ipt=d191eb7a7592ede00a4efb69d08105b59832ac04e24f4d6e8eab7df59bc5537b&ipo=images",
+    feature: true,
   },
   {
     title: "film 7",
-    director: "director 7",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    genre: { name: "thriller", description: "genre description here" },
+    director: {
+      name: "director 1",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      born: 1940,
+      death: "",
+    },
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foriginalvintagemovieposters.com%2Fwp-content%2Fuploads%2F2018%2F06%2FStar-Wars-5672-768x1162.jpg&f=1&nofb=1&ipt=d191eb7a7592ede00a4efb69d08105b59832ac04e24f4d6e8eab7df59bc5537b&ipo=images",
+    feature: true,
   },
   {
     title: "film 8",
-    director: "director 8",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    genre: { name: "thriller", description: "genre description here" },
+    director: {
+      name: "director 1",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      born: 1940,
+      death: "",
+    },
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foriginalvintagemovieposters.com%2Fwp-content%2Fuploads%2F2018%2F06%2FStar-Wars-5672-768x1162.jpg&f=1&nofb=1&ipt=d191eb7a7592ede00a4efb69d08105b59832ac04e24f4d6e8eab7df59bc5537b&ipo=images",
+    feature: true,
   },
   {
     title: "film 9",
-    director: "director 9",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    genre: { name: "thriller", description: "genre description here" },
+    director: {
+      name: "director 1",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      born: 1940,
+      death: "",
+    },
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foriginalvintagemovieposters.com%2Fwp-content%2Fuploads%2F2018%2F06%2FStar-Wars-5672-768x1162.jpg&f=1&nofb=1&ipt=d191eb7a7592ede00a4efb69d08105b59832ac04e24f4d6e8eab7df59bc5537b&ipo=images",
+    feature: true,
   },
   {
     title: "film 10",
-    director: "director 10",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    genre: { name: "thriller", description: "genre description here" },
+    director: {
+      name: "director 1",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      born: 1940,
+      death: "",
+    },
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Foriginalvintagemovieposters.com%2Fwp-content%2Fuploads%2F2018%2F06%2FStar-Wars-5672-768x1162.jpg&f=1&nofb=1&ipt=d191eb7a7592ede00a4efb69d08105b59832ac04e24f4d6e8eab7df59bc5537b&ipo=images",
+    feature: true,
   },
 ];
 
@@ -60,21 +170,132 @@ app.use(morgan("combined", { stream: accessLogStream }));
 //shortcut so i dont have to res.send() all files in the public folder (right now just documentation.html)
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  let responseText = "Welcome to my app!";
-  res.send(responseText);
+//MOVIES
+app.get("/", (request, response) => {
+  let responseText = "It's Movie Night !";
+  response.send(responseText);
 });
 
-app.get("/movies", (req, res) => {
-  res.json(moviesList);
+//Return a list of ALL movies to the user;
+app.get("/movies", (request, response) => {
+  response.status(200).json(movies);
 });
 
-// app.get("/secreturl", (req, res) => {
-//   let responseText = "This is a secret url with super top-secret content.";
-//   responseText += "<small>Requested at: " + req.requestTime + "</small>";
-//   res.send(responseText);
-// });
+// Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user;
 
+app.get("/movies/:title", (request, response) => {
+  const { title } = request.params;
+  const movie = movies.find((movie) => {
+    return movie.title === title;
+  });
+
+  if (movie) {
+    response.status(200).json(movie);
+  } else {
+    response.status(400).send("no such movie was found");
+  }
+});
+//Return data about a genre (description) by name/title (e.g., “Thriller”);
+app.get("/movies/genres/:genreName", (request, response) => {
+  const { genreName } = request.params;
+  const MovieInGenre = movies.find((movie) => {
+    return movie.genre.name === genreName;
+  });
+
+  if (MovieInGenre) {
+    const genre = MovieInGenre.genre;
+
+    response.status(200).json(genre);
+    console.log(genre);
+  } else {
+    response.status(400).send("no such genre found");
+  }
+});
+
+// Return data about a director (bio, birth year, death year) by name;
+app.get("/movies/directors/:directorName", (request, response) => {
+  const { directorName } = request.params;
+
+  const directorMovie = movies.find((movie) => {
+    return movie.director.name === directorName;
+  });
+
+  if (directorMovie) {
+    const director = directorMovie.director;
+    response.status(200).json(director);
+  } else {
+    response.status(401).send("no such director found!");
+  }
+});
+
+//USERS
+// Allow new users to register;
+app.post("/users", (request, response) => {
+  const newUser = request.body;
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    response.status(201).json(newUser);
+  } else {
+    response.status(400).send("missing name");
+  }
+});
+
+//update user
+app.put("/users/:id", (request, response) => {
+  const { id } = request.params;
+  const updateUser = request.body;
+  let user = users.find((user) => user.id.toString() === id);
+
+  if (user) {
+    user.name = updateUser.name;
+    response.status(200).json(user);
+  } else {
+    response.status(400).send("no such user");
+  }
+});
+
+//add favorite movie to users list
+app.post("/users/:id/:movieTitle", (request, response) => {
+  const { id, movieTitle } = request.params;
+  let user = users.find((user) => user.id.toString() === id);
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    response
+      .status(200)
+      .send(`${movieTitle} has been added to user ${id}s list`);
+  } else {
+    response.status(400).send("no such movie");
+  }
+});
+//deletes favorite movie to users list
+app.delete("/users/:id/:movieTitle", (request, response) => {
+  const { id, movieTitle } = request.params;
+  let user = users.find((user) => user.id.toString() === id);
+
+  if (user) {
+    user.favoriteMovies.filter((title) => title !== movieTitle);
+    response
+      .status(200)
+      .send(`${movieTitle} has been removed from user ${id}s list`);
+  } else {
+    response.status(400).send("no such movie");
+  }
+});
+//deletes user
+app.delete("/users/:id/", (request, response) => {
+  const { id } = request.params;
+  let user = users.find((user) => user.id.toString() === id);
+
+  if (user) {
+    users = users.filter((user) => user.id.toString() !== id);
+    response.status(200).send(`user ${id} has been deleted`);
+    // response.json(users);
+  } else {
+    response.status(400).send("no such movie");
+  }
+});
 //error handling middleware function
 //should be last, but before app.listen()
 app.use((err, req, res, next) => {
