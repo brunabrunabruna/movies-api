c28211a0a5bd245a5795f65a256f7a5e818cc18a
brunabrunabruna <brunaandreis@hotmail.com>
2023-09-05 11:55:09 +0200

    mongoose connect modified

--- index.js
+++ index.js
@@ -21,10 +21,14 @@ const Users = Models.User;
 // });
 
 //connects to online mongoDB database -- first param is an environment variable, so the data about my mongodb(username,pw) is not exposed on my github
-mongoose.connect(process.env.CONNECTION_URI, {
+mongoose.connect("https://movies-api-render-0a0q.onrender.com/", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+// mongoose.connect(process.env.CONNECTION_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 const app = express();
 app.use(bodyParser.json());
